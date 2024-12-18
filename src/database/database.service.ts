3fcaff45fa8d6540ea78b21a0806ad24ce9e5fb2
yanga17 <yangagovuza@gmail.com>
2024-12-18 08:15:04 +0200

    removed database console

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -6,13 +6,6 @@ export class DatabaseService implements OnModuleInit {
   private pool: mysql.Pool;
 
   constructor() {
-    console.log('Database Config:');
-    console.log('MYSQL PORT:', process.env.PORT);
-    console.log('HOST:', process.env.HOST);
-    console.log('USER:', process.env.USER);
-    console.log('PASS:', process.env.PASSWORD);
-    console.log('DATABASE:', process.env.DATABASE);
-
     this.pool = mysql.createPool({
       host: process.env.HOST || 'localhost', // Default to 'localhost' if undefined
       user: process.env.USER || 'root',
