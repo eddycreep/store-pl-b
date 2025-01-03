887f3fe756b4b9b7f2c91ba0aeecb24217a9daab
yanga17 <yangagovuza@gmail.com>
2025-01-03 14:12:03 +0200

    configService in main

--- dist/main.js
+++ dist/main.js
@@ -4,10 +4,12 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const dotenv = require("dotenv");
 const swagger_1 = require("./utils/swagger");
+const config_1 = require("@nestjs/config");
 dotenv.config();
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    const port = parseInt(process.env.PORT, 10) || 4000;
+    const configService = app.get(config_1.ConfigService);
+    const port = configService.getOrThrow('PORT');
     app.enableCors({
         origin: 'http://localhost:4000',
         methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
