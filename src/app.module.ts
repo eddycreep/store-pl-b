06b7a6bd914fce7ab755bd65b5e513867d9d4cc4
yanga17 <yangagovuza@gmail.com>
2025-01-06 14:00:42 +0200

    app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { UsersModule } from './users/users.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-//import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 //import { ProductsModule } from './products/products.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
